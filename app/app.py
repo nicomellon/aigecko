b5531c65f4c899101db0c18798f1065738538a5b
nicomellon <nmellon94@gmail.com>
2022-02-16 13:33:28 +0100

    adds html templates

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return '<h1>Hello world!</h1>'
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
