102d6257d54411e39096a71105e3427239677ca8
nicomellon <nmellon94@gmail.com>
2022-02-16 15:43:22 +0100

    adds post /upload_image

--- app/app.py
+++ app/app.py
@@ -2,15 +2,38 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+images = [
+    "1", "2"
+]
 
+
+# app homepage
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", images=images)
 
 
-@app.route("/upload_image")
+# get the image provided by the user and store it
+@app.route("/upload_image", methods=["POST"])
 def upload_image():
-    return '<h1>upload</h1>'
+    name = request.form["name"]
+
+    if name:
+        return f"Hi {name}"
+    else:
+        return "Hi"
+
+
+# read a previously stored image and return its height and width
+@app.route("/analyse_image")
+def analyse_image():
+    return '<h1>analyse</h1>'
+
+
+# list all the images uploaded so far as well as their image ids
+@app.route("/list_images")
+def list_images():
+    return '<h1>list images</h1>'
 
 
 if __name__ == "__main__":
