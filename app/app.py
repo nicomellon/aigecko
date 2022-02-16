4829c58a906da0884c6f9f2c36b041daa99d9120
nicomellon <nmellon94@gmail.com>
2022-02-16 13:52:33 +0100

    adds jinja layout

--- app/app.py
+++ app/app.py
@@ -8,5 +8,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/upload_image")
+def upload_image():
+    return '<h1>upload</h1>'
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
