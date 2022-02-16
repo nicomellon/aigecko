6d173eae5c4df4472f8bee5421b757abda60aab0
nicomellon <nmellon94@gmail.com>
2022-02-16 16:56:25 +0100

    adds file upload

--- app/app.py
+++ app/app.py
@@ -1,27 +1,49 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, flash, render_template, request, redirect
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.getcwd() + "/uploads"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-images = [
-    "1", "2"
-]
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 # app homepage
 @app.route("/")
 def index():
-    return render_template("index.html", images=images)
+    return render_template("index.html")
 
 
 # get the image provided by the user and store it
 @app.route("/upload_image", methods=["POST"])
 def upload_image():
-    name = request.form["name"]
+    # check if the post request has the file part
+    if "file" not in request.files:
+        # flash("No file part")
+        return 'invalid file'
+        # return redirect("/")
 
-    if name:
-        return f"Hi {name}"
-    else:
-        return "Hi"
+    file = request.files['file']
+    
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        # flash('No selected file')
+        return redirect("/")
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return f'valid file going to {UPLOAD_FOLDER}'
+        # return redirect("/")
+
+    return 'invalid file'
 
 
 # read a previously stored image and return its height and width
